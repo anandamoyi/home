dbd93f7025a739125813de49a21ccb61a5d60c0e
anandamoyi <anandamoyi_saha@hotmail.com>
2022-05-17 19:53:20 -0400

    Made custom changes

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,31 +6,31 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Anandamoyi",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Saha",
+  message: " Passionate about solving real world problems with computer engineering and new technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/anandamoyi",
     },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
+    //{
+     // image: "fa-facebook",
+      //url: "https://www.facebook.com/hashirshoaeb",
+    //},
+   // {
+     // image: "fa-instagram",
+      //url: "https://www.instagram.com/hashirshoaeb/",
+   // },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/anandamoyi-saha-092528221/",
     },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
+    //{
+      //image: "fa-twitter",
+      //url: "https://www.twitter.com/hashirshoaeb/",
+    //},
   ],
 };
 
@@ -51,11 +51,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/anonymous.png"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "My name is Anandamoyi Saha. I’m an undergraduate student in my 4th year at McGill University. I am studying for a Bachelor's degree in Computer Engineering. I'm most passionate about learning new technologies and my goal is to use computer engineering to solve real world problems.",
+  resume: "https://docs.google.com/document/d/1HqhjAlXd-D6S2Q4ZVu39z4jzpex51TfEbxCMIsVMdTo/edit",
 };
 
 // PROJECTS SECTION

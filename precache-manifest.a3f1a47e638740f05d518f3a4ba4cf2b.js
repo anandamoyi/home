99a73292bb303ea91bb568ab8d6db3c4792e76a8
anandamoyi <anandamoyi_saha@hotmail.com>
2022-05-17 21:23:56 -0400

    Updates

---
+++ precache-manifest.a3f1a47e638740f05d518f3a4ba4cf2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ad77ca5e4f3f08443691038e0d647f",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "9e11bd9a8e51ac5dac67",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94ef74be75a26eee0bde",
+    "url": "/home/static/js/2.d1b0d4b7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.d1b0d4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e11bd9a8e51ac5dac67",
+    "url": "/home/static/js/main.5a448acd.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "8c0a2a99cb42d9555ea7d8d5c6611140",
+    "url": "/home/static/media/resume.8c0a2a99.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.a3f1a47e638740f05d518f3a4ba4cf2b.js"
 );
 
 self.addEventListener('message', (event) => {
